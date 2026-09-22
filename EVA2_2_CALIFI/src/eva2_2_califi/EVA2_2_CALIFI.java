586c6db1ba324eba7a82cdf26c7a16615ba554f4
Mtz002 <L26550272@chihuahua2.TECNM.MX>
2026-09-21 18:47:45 -0600

    Add files via upload

---
+++ EVA2_2_CALIFI/src/eva2_2_califi/EVA2_2_CALIFI.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_2_califi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA2_2_CALIFI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edad;
+           Scanner scanner = new Scanner(System.in);
+           System.out.println("Cual es tu Califíca"
+                   + 
+                
+             edad=scanner.nextInt();
+             if (edad >= 18) {
+                 System.out.println("Si puedes entrar pedazo de viejo");
+             }
+             else{
+                 System.out.println("No puedes entrar bebo");
+             }
+  
