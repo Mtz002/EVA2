0270ab61b57e1ed21d1fd758c1db721aadc09911
Mtz002 <L26550272@chihuahua2.TECNM.MX>
2026-09-23 18:54:47 -0600

    Add files via upload

---
+++ EVA2_8_MENU/src/eva2_8_menu/EVA2_8_MENU.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_menu;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA2_8_MENU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
